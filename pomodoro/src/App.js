e111aac96dbe4cc92b290f8227fd45c3c0b5f307
Michael Trew <40902786+mtrew2015@users.noreply.github.com>
2019-12-22 05:41:40 -0500

    working on audio

--- pomodoro/src/App.js
+++ pomodoro/src/App.js
@@ -3,30 +3,36 @@ import React, { useState, useEffect } from 'react';
 import './App.scss';
 
 function App() {
-	const [ time, setTime ] = useState(1500);
+	const [ time, setTime ] = useState(10)
 	const [ active, setActive ] = useState(false);
 	const [ count, setCount ] = useState(0);
-	const [ onBreak, setOnBreak ] = useState(false);
+  const [ onBreak, setOnBreak ] = useState(false);
+  
+
 
 	useEffect(() => {
-		let int = null;
+    // const audio = document.getElementsByClassName('audio-element')
+    // console.log(audio)
+    let int = null;
+    document.title = onBreak ? `Break: ${time}`: `${time} Left`
 		if (active && time > 0) {
 			int = setInterval(() => {
 				setTime((time) => time - 1);
-			}, 10);
+			}, 1000);
 		} else {
       if (active && time === 0 && !onBreak){
         setActive(false);
         setTime(shortOrLong())
         setOnBreak(true)
-        setCount(count + 1)
-        alert("Time For A Break!!")
+        setCount(count + 1);
+        audio.play()
       }
       else if (onBreak && time === 0){
         setOnBreak(false);
         setTime(1500);
         clearInterval(int);
         setActive(false)
+        // audio.play()
       }
     }
 		return () => clearInterval(int);
@@ -49,6 +55,7 @@ function App() {
 
 	return (
 		<div className="App">
+      {/* <audio className="audio-element" src='./assets/bell.mp3'/> */}
 			<h1>Pomodoro Timer</h1>
 			<p>{time}</p>
 			<p>{count}</p>
