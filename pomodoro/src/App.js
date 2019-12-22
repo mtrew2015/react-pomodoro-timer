3a9fcb8e231e93d789bfd0797124b45ad9aaf77d
Michael Trew <40902786+mtrew2015@users.noreply.github.com>
2019-12-22 05:52:14 -0500

    audio works

--- pomodoro/src/App.js
+++ pomodoro/src/App.js
@@ -3,16 +3,21 @@ import React, { useState, useEffect } from 'react';
 import './App.scss';
 
 function App() {
-	const [ time, setTime ] = useState(10)
+	const [ time, setTime ] = useState(1500)
 	const [ active, setActive ] = useState(false);
 	const [ count, setCount ] = useState(0);
   const [ onBreak, setOnBreak ] = useState(false);
   
-
+ const audio = new Audio('/bell.mp3');
 
 	useEffect(() => {
-    // const audio = document.getElementsByClassName('audio-element')
-    // console.log(audio)
+    const shortOrLong = () => {
+      if (count % 2 !=0){
+        return 600
+      } else{
+        return 300;
+      }
+    }
     let int = null;
     document.title = onBreak ? `Break: ${time}`: `${time} Left`
 		if (active && time > 0) {
@@ -25,18 +30,18 @@ function App() {
         setTime(shortOrLong())
         setOnBreak(true)
         setCount(count + 1);
-        // audio.play()
+        audio.play()
       }
       else if (onBreak && time === 0){
         setOnBreak(false);
         setTime(1500);
         clearInterval(int);
         setActive(false)
-        // audio.play()
+        audio.play()
       }
     }
 		return () => clearInterval(int);
-	});
+	}, [onBreak, time, active, count, audio]);
 
 	const formatTime = () => {
 		let minutes = Math.floor(time / 60);
@@ -44,13 +49,7 @@ function App() {
 		return `${minutes.toString().padStart(2, 0)}:${seconds.toString().padStart(2, 0)}`;
   };
   
-  const shortOrLong = () => {
-    if (count % 2 !=0){
-      return 600
-    } else{
-      return 300;
-    }
-  }
+ 
 
 
 	return (
